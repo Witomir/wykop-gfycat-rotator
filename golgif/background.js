46773b8bd2afc6047fb1a3f3312f3c22dec0b073
witomir <witomir.badowski@gmail.com>
2015-10-25 09:24:56 +0100

    komentarze do funkcji

--- golgif/background.js
+++ golgif/background.js
@@ -1,4 +1,11 @@
 function injectScript(clickedTab) {
+    /**
+     * Otwieramy nowa kartę w kórej będą rotowały się gify i wstrzykujemy na nią skrypt rozszerzenia
+     * Musi być to strona gfycat.com żeby zastąpić jej content przez iframe w którym będą odtwarzały się
+     * gify z tej domeny. Dzięki temu można manipulować skryptami wewnątrz iframe (np. zrobić double click na gifie,
+     * żeby go powiększyć na fullscreen) a przeglądarka nie będzie zgłaszać zagrożenia bezpieczeństwa
+     * @link https://en.wikipedia.org/wiki/Same-origin_policy
+     */
 	var newURL = "http://gfycat.com";
     chrome.tabs.create({url: newURL}, function (tab) {
 		chrome.tabs.executeScript(tab.id, {
--- golgif/gfycat.js
+++ golgif/gfycat.js
@@ -1,27 +1,85 @@
+/**
+ * Pobiera wpisy z mirko spod podanego tagu, szuka w nich linków do gfycat.com i rotuje
+ * znalezione gify. Po zakończeniu kolejki, pobiera nowe dane i dalej rotuje.
+ * @param appKey
+ * @param tag
+ * @constructor
+ */
 var App = function(appKey, tag){
+
+    /**
+     * twój klucz API
+     */
     var key = appKey;
+
+    /**
+     * nazwa tagu wykopowego spod którego będziemy pobierać gify
+     */
     var wykopTag = tag;
+
+    /**
+     * Ilość stron wpisów do pobrania z mirko
+     */
+    var maxApiPages = 4;
+
+    /**
+     * Co jaki czas przełączać gify
+     */
+    var repeatFrequency = 20 * 1000;
+
     var apiSrc = 'http://a.wykop.pl/';
     var gfycatApiUrl = 'http://gfycat.com/cajax/get/';
     var apiUri = apiSrc + 'tag/' + 'Entries/' + wykopTag + '/appkey,' + key + ',page,';
     var gfycatRegex = /http\:\/\/gfycat\.com\/[a-zA-Z]*/g;
     var gfycatNameRegex = /gfycat\.com\/([a-zA-Z]*)/g;
     var gifs = [];
-    var maxApiPages = 6;
     var processedPages = 0;
     var iframe = {};
-    var repeatFrequency = 20 * 1000;
     var appSheduledJob;
     var isStarted = false;
+    var stopApp = {};
 
-    (function prepareIframe(){
+    /**
+     * @constructor
+     */
+    function App(){
+        clearGfys();
+        prepareIframe();
+        loadData(1, maxApiPages);
+    };
+    App();
+
+    /**
+     * Publiczna funkcja którą można sprawdzić, czy apka rozpoczęła już rotację filmików
+     * @returns {boolean}
+     */
+    this.isStarted = function(){
+        return isStarted;
+    }
+
+    /**
+     * Publiczna funkcja, która zatrzymuje rotację filmików
+     */
+    this.stopApp = stopApp = function(){
+        isStarted = false;
+        clearTimeout(appSheduledJob);
+    }
+
+    /**
+     * Tworzymy iframe do którego będą ładowane gify i rozpychamy go na całą stronę
+     */
+    function prepareIframe(){
         iframe = document.createElement('iframe');
         iframe.id = 'gfyiframe';
         iframe.style.width = '100%';
         iframe.style.height = '100%';
-    })();
+    };
 
-    var doubleClickGfy =  function (argument) {
+    /**
+     * Dwukrotnie klika w video, żeby się dopasowało do rozmiaru okna
+     * @param event
+     */
+    var doubleClickGfy = function (event) {
         var iframeDocument = iframe.contentDocument;
         var video = iframeDocument.getElementById('gfyVid1');
         var event = new MouseEvent('dblclick', {
@@ -32,10 +90,26 @@ var App = function(appKey, tag){
         video.dispatchEvent(event);
     }
 
+    /**
+     * Pobiera wpisy spod podanego aplikacji tagu
+     * @param page
+     * @param maxPages
+     */
+    function loadData(page, maxPages){
+        for(; page <= maxPages; page++) {
+            doAjax(apiUri + page, parseWykopResponse);
+        }
+    };
+
+    /**
+     * Wołana gdy już załadują się dane i można zacząć rotować filmiki
+     * @param index
+     */
     function startApp(index) {
         isStarted = true;
         if (index == gifs.length) {
-            index = 0;
+            stopApp();
+            return App();
         }
 
         iframe.src = gifs[index]['url'];
@@ -48,35 +122,35 @@ var App = function(appKey, tag){
         appSheduledJob = setTimeout(function(){startApp(index)}, timeout);
     }
 
-
-    this.isStarted = function(){
-        return isStarted;
-    }
-
-    this.stopApp = function(){
-        isStarted = false;
-        clearTimeout(appSheduledJob);
-    }
-
+    /**
+     * Parsuje odpowiedź API gfycat.com
+     * @param response
+     */
     function parseGfycatResponse(response) {
         if(this.readyState == XMLHttpRequest.DONE && this.status==200) {
             response = JSON.parse(this.responseText);
-            if(response.items !== null && response.items !== undefined){
-                for(var i = 0; i < response.items.length; i++){
-                    prepareEntry(response.items[i]);
-                }
-            }
         }
     }
 
+    /**
+     * Użytwane do uzupełnienia informacji o pliku gfy, np. określenie jego długości, żeby nie
+     * przełączać do następnego filmiku w kolejce jeszcze w trakcie odtwarzania poprzedniego gifa.
+     * Czasami moment przełączenia jest dosłownie przed uderzeniem zdobywającym punkt, co nieco wkurza
+     * #piekloperfekcjonistow
+     * @param url
+     * @param gfyInfo
+     */
     function fetchGfyInfo(url, gfyInfo) {
         var gfycatNameRegex = /gfycat\.com\/([a-zA-Z]*)/g;
         var gfyName = gfycatNameRegex.exec(url);
-        gfyName = gfyName[1];
-
+        //gfyName = gfyName[1];
         //doAjax(gfycatApiUrl + gfyName, parseGfycatResponse);
     }
 
+    /**
+     * Dodaje linka do gfy do puli linków z których będzie korzystała aplikacja
+     * @param url
+     */
     function addGfy(url) {
         gfyInfo = {
             'url': url,
@@ -86,12 +160,24 @@ var App = function(appKey, tag){
         gifs.push(gfyInfo);
     }
 
+    /**
+     * Czyści kolejkę gifów
+     */
+    function clearGfys(){
+        gifs = [];
+    }
+
+    /**
+     * Bardzo uproszczony pobieracz AJAXowy
+     * @param url
+     * @param responseHandler
+     */
     function doAjax(url, responseHandler) {
         var url = url || "";
         var method =  "GET";
         var async =  true;
 
-        if (url != "") {
+        if (url !== "") {
             var xmlHttpRequst = new XMLHttpRequest();
             xmlHttpRequst.open(method, url, async);
             xmlHttpRequst.onreadystatechange = responseHandler;
@@ -99,6 +185,10 @@ var App = function(appKey, tag){
         }
     }
 
+    /**
+     * Szuka linków do gfycatów w treści wpisu i w załączniku
+     * @param string entry treść wpisu
+     */
     function prepareEntry(entry) {
         var gfysInEntrysBody = gfycatRegex.exec(entry.body);
         if(gfysInEntrysBody && gfysInEntrysBody.length > 0){
@@ -117,6 +207,10 @@ var App = function(appKey, tag){
         }
     }
 
+    /**
+     * Parsuje JSON od wykopu i jeśli wczytała się ostatnia ze stron, startuje rotowanie gifów
+     * @param response
+     */
     function parseWykopResponse(response) {
         if(this.readyState == XMLHttpRequest.DONE && this.status==200) {
             processedPages++;
@@ -131,15 +225,6 @@ var App = function(appKey, tag){
             }
         }
     }
-
-
-    function loadData(page, maxPages){
-        for(; page <= maxPages; page++) {
-            doAjax(apiUri + page, parseWykopResponse);
-        }
-    };
-
-    loadData(1, maxApiPages);
 }
 
 if(GolgifApp === undefined || GolgifApp.isStarted() === false) {
